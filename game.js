f5fe52d00a0f7627f319cadc8fbe56b833366e26
Alex <adalgleish95@gmail.com>
2016-10-02 23:05:27 +0100

    Create score counter (for #2)

--- game.js
+++ game.js
@@ -1,16 +1,25 @@
 $(".box").hide();
 
 var boxes = document.getElementsByClassName("box");
+var score = 0;
+// to stop multiple clicks 
+var canClick = false;
 
 function scanner() {
 
   var chance = Math.floor(Math.random()*9)
   $(boxes[chance]).show();
+  canClick = true;
 }
 
 scanner();
 
- $(".box").on("click", function() {
-  	$(this).hide();
+$(".box").on("click", function() {
+  if(canClick){
+    canClick = false;
+    $(this).hide();
+    score++;
+    $("#score").text(score);
     setInterval(scanner(), 2000);
-  });
+  }
+});
